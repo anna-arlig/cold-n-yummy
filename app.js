61cacdb7a2834e4095402c13d8172ff3b10c8d00
anna.arlig <you@example.com>
2022-03-30 15:48:21 +0200

    Add voting page and styling

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ app.use(session({
     res.render('pages/vote', {flavors})
   })
 
+  app
+
   const PORT = process.env.PORT || 5000
   app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
